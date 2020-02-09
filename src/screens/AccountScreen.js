5dc1b51c581b2ee058c181a877199108556f49ca
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-08 21:21:55 -0800

    add boilerplate for different screens.

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const AccountScreen = () => {
+  return <Text style={{ fontSize: 48 }}>Account Screen</Text>;
+};
+
+const styles = Stylesheet.create({});
+
+export default AccountScreen;
