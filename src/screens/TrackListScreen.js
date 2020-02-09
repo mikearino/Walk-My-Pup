53d098c5ac7b90855f27fce3f71b0aa9ba95c726
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-08 22:45:10 -0800

    finish wiring up navigator.

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const TrackListScreen = () => {
-  return <Text style={{ fontSize: 48 }}>Track List Screen</Text>;
+const TrackListScreen = ({ navigation }) => {
+  return (
+    <>
+      <Button
+        title="Go to Track Detail"
+        onPress={() => {
+          navigation.navigate('TrackDetail');
+        }}
+      />
+      <Text style={{ fontSize: 48 }}>Track List Screen</Text>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
