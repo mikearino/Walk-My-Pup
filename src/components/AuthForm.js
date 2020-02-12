8a8e1cc3f6c4782d6b29a07aec07a7a38eedd756
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-11 21:26:01 -0800

    refactoring code into authForm.

---
+++ src/components/AuthForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+//react-native-elements is a library with built up components
+import { Text, Button, Input } from 'react-native-elements';
+
+const AuthForm = () => {};
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+const styles = StyleSheet.create({});
+
+export default AuthForm;
