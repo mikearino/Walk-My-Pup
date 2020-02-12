dc2e39ca6d1e61f1eb80685654e4639a4f2be91e
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-11 21:33:42 -0800

    more work on moving code.

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -3,10 +3,43 @@ import { View, StyleSheet } from 'react-native';
 //react-native-elements is a library with built up components
 import { Text, Button, Input } from 'react-native-elements';
 
-const AuthForm = () => {};
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+const AuthForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  return (
+    <>
+      {/* Children of spacer has styling. */}
+      <Spacer>
+        <Text h3>Sign up with WalkMyPup!</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
+      <Spacer>
+        {/* Passing in email and password to sign up which is de-structured from 
+        AuthContext, which in turn will run the reducer. */}
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
+      </Spacer>
+    </>
+  );
+};
 const styles = StyleSheet.create({});
 
 export default AuthForm;
