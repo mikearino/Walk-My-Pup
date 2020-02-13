2710fcc4f2c8152e985ad36506854d5474d6c179
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-12 17:35:03 -0800

    begin to add map in to create track view.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import Map from '../components/Map';
+import { SafeAreaView } from 'react-navigation';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>Track Create Screen</Text>;
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>Create a track</Text>
+      <Map />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
