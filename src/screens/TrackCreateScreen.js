bf2627b6c80f110d6da93d264ddd8581bdef286d
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-12 20:42:00 -0800

    work on expo-location for tracking.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import Map from '../components/Map';
 import { SafeAreaView } from 'react-navigation';
-
+//Asks user for permission to track.
+import { requestPermissionsAsync } from 'expo-location';
 const TrackCreateScreen = () => {
+  //For displaying error to user
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>Create a track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
