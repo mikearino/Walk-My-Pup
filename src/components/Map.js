ed2056d5795960840508c3ed66081cbab08fdc3b
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-12 17:53:28 -0800

    building map with props.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 const Map = () => {
-  return <Text>I am a map</Text>;
+  //Map has to have at least a height to show up.
+  return <MapView style={styles.map} 
+  //What to show when map is rendered.
+  initialRegion={{
+    //What location.
+    latitude:
+    longitude:
+    //Zoom level
+    latitudeDelta:
+    longitudeDelta:
+  }}
+  />;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300
+  }
+});
 
 export default Map;
