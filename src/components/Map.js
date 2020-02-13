fbd8e6275b90d8ce80b658c3ff13c9ca68c3b91f
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-12 17:57:04 -0800

    add in Portlnad coords.

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,17 +4,20 @@ import MapView from 'react-native-maps';
 
 const Map = () => {
   //Map has to have at least a height to show up.
-  return <MapView style={styles.map} 
-  //What to show when map is rendered.
-  initialRegion={{
-    //What location.
-    latitude:
-    longitude:
-    //Zoom level
-    latitudeDelta:
-    longitudeDelta:
-  }}
-  />;
+  return (
+    <MapView
+      style={styles.map}
+      //What to show when map is rendered.
+      initialRegion={{
+        //What location.
+        latitude: 45.512794,
+        longitude: -122.679565,
+        //Zoom level
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
