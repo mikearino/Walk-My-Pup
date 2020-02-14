768ff7deb02c4197127094ed34df56b46ae4a2bd
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-13 23:57:41 -0800

    delete somefunctionality so map does not keep centering.

--- src/components/Map.js
+++ src/components/Map.js
@@ -18,9 +18,9 @@ const Map = () => {
     <MapView
       style={styles.map}
       //What to show when map is rendered.
+      //What location gets rendered when map first gets
+      //shown on the screen.
       initialRegion={{
-        //What location gets rendered when map first gets
-        //shown on the screen.
         ...currentLocation.coords,
         //Zoom level
         latitudeDelta: 0.01,
@@ -28,11 +28,11 @@ const Map = () => {
       }}
       //Prop to track the user around and automatically
       //re-center itself as well.
-      region={{
-        ...currentLocation.coords,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01
-      }}
+      // region={{
+      //   ...currentLocation.coords,
+      //   latitudeDelta: 0.01,
+      //   longitudeDelta: 0.01
+      // }}
     >
       <Circle
         //Exact lat/long to draw a circle on the map.
