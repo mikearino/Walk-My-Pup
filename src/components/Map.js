73999d4207eb33868b8d7cd8c7bbae969c0180cb
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-13 21:25:27 -0800

    bug fix for ActivityIndicator on props.

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,7 @@ const Map = () => {
   } = useContext(LocationContext);
   //If the current location is not yet available, show a spinner.
   if (!currentLocation) {
-    return <ActivityIndicator size="large" style={{ martginTop 200 }}/>
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
 
   return (
@@ -19,14 +19,20 @@ const Map = () => {
       style={styles.map}
       //What to show when map is rendered.
       initialRegion={{
-        //What location.
+        //What location gets rendered when map first gets
+        //shown on the screen.
         ...currentLocation.coords,
         //Zoom level
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-      //Prop to track the user around.
-      region={{}}
+      //Prop to track the user around and automatically
+      //re-center itself as well.
+      region={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
     ></MapView>
   );
 };
