62f219629fb61ba749f160175439c548e85d7c01
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-14 22:48:26 -0800

    start custom hook for location.

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import {
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync
+} from 'expo-location';
+
+//This is a custom hook.
+export default () => {};
