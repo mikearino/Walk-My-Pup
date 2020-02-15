432bf9be04507b03a54ed057738b239df0deb68d
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-14 22:55:51 -0800

    migrate start watching function to custom hook.

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -6,4 +6,32 @@ import {
 } from 'expo-location';
 
 //This is a custom hook.
-export default () => {};
+export default () => {
+  //For displaying error to user
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      //Asks user for permission to track.
+      await requestPermissionsAsync();
+      //Watch users location and see it change over time
+      await watchPositionAsync(
+        {
+          //How accurate do you want, be mindful of bat power.
+          accuracy: Accuracy.BestForNavigation,
+          //Updated every second
+          timeInterval: 1000,
+          //Or every ten meters
+          distanceInterval: 10
+        },
+        //Callback gets invoked every single time a user changes
+        //location with location object.
+        location => {
+          addLocation(location);
+        }
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
+};
