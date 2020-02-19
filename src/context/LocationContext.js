30d24dd6818e110e00430af9d304299a116e6e6e
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-19 13:09:36 -0800

    add in reducer functionality for recording.

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -6,6 +6,17 @@ const locationReducer = (state, action) => {
       //Update state, with new currentLocation object value of
       //action.payload
       return { ...state, currentLocation: action.payload };
+    case 'start_recording':
+      // Update recording flag to true.
+      return { ...state, recording: true };
+    case 'stop_recording':
+      return { ...state, recording: false };
+    case 'add_location':
+      //Take all of the present locations and throw them
+      //into a new array. Then add a new action payload as well.
+      return { ...state, locations: [...state.locations, action.payload] };
+    case 'change_name':
+      return { ...state, name: action.payload };
     default:
       return state;
   }
@@ -13,12 +24,23 @@ const locationReducer = (state, action) => {
 
 //One function that gets returned with dispatch to run
 //another function.
-const startRecording = dispatch => () => {};
-const stopRecording = dispatch => () => {};
-const addLocation = dispatch => location => {
+//Receives whatever the name has been updated to.
+const ChangeName = dispatch => name => {
+  dispatch({ type: 'change_name', payload: name });
+};
+const startRecording = dispatch => () => {
+  dispatch({ type: 'start_recording' });
+};
+const stopRecording = dispatch => () => {
+  dispatch({ type: 'stop_recording' });
+};
+const addLocation = dispatch => (location, recording) => {
   //Dispatch current location from TrackCreateScreen's location
   //callback.
   dispatch({ type: 'add_current_location', payload: location });
+  if (recording) {
+    dispatch({ type: 'add_location', payload: location });
+  }
 };
 
 export const { Context, Provider } = createDataContext(
@@ -26,7 +48,8 @@ export const { Context, Provider } = createDataContext(
   {
     startRecording,
     stopRecording,
-    addLocation
+    addLocation,
+    ChangeName
   },
-  { recording: false, locations: [], currentLocation: null }
+  { name: '', recording: false, locations: [], currentLocation: null }
 );
