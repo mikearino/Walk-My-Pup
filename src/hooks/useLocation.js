ef3e98ba9e84e4797072a5d87c934766eb1b7422
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-20 10:22:01 -0800

    clean up subscriber.

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -11,44 +11,44 @@ import {
 export default (shouldTrack, callback) => {
   //For displaying error to user
   const [err, setErr] = useState(null);
-  //When hook runs for the very first time, it's saying we don't have a subscriber
-  //whatsoever, it doesn't exist because it's not watching
-  const [subscriber, setSubscriber] = useState(null);
-  const startWatching = async () => {
-    try {
-      //Asks user for permission to track.
-      await requestPermissionsAsync();
-      //Watch users location and see it change over time.
-      //Subscriber has a function called remove which will
-      //make sure to stop tracking the users location.
-      const sub = await watchPositionAsync(
-        {
-          //How accurate do you want, be mindful of bat power.
-          accuracy: Accuracy.BestForNavigation,
-          //Updated every second
-          timeInterval: 1000,
-          //Or every ten meters
-          distanceInterval: 10
-        },
-        //Callback gets invoked every single time a user changes
-        //location.
-        callback
-      );
-      setSubscriber(sub);
-    } catch (e) {
-      setErr(e);
-    }
-  };
+
   //By putting a value inside of the array, you are telling a react
   //to run this function more than once the first time the component
   //rendered to the screen. If the value of should track changes, then
   //react will run startWatching again.
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        //Asks user for permission to track.
+        await requestPermissionsAsync();
+        //Watch users location and see it change over time.
+        //Subscriber has a function called remove which will
+        //make sure to stop tracking the users location.
+        const subscriber = await watchPositionAsync(
+          {
+            //How accurate do you want, be mindful of bat power.
+            accuracy: Accuracy.BestForNavigation,
+            //Updated every second
+            timeInterval: 1000,
+            //Or every ten meters
+            distanceInterval: 10
+          },
+          //Callback gets invoked every single time a user changes
+          //location.
+          callback
+        );
+      } catch (e) {
+        setErr(e);
+      }
+    };
     if (shouldTrack) {
       startWatching();
     } else {
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
     return () => {
       if (subscriber) {
