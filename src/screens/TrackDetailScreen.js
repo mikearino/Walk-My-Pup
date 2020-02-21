6d96fd35f78a28e944f641fc96774e0ad278a71d
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-21 15:34:29 -0800

    passing in id's through .find.

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Context as TrackContext } from '../context/TrackContext';
 
-const TrackDetailScreen = () => {
-  return <Text style={{ fontSize: 48 }}>Track Detail Screen</Text>;
+const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  //This variable identify what element was just tapped on.
+  const _id = navigation.getParam('_id');
+
+  //.find returns the first truthy value and saves it into track.
+  const track = state.find(t => t._id === _id);
+
+  return <Text style={{ fontSize: 48 }}>{track.name}</Text>;
 };
 
 const styles = StyleSheet.create({});
