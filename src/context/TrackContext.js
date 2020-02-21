3db9f051d21ce39b66cd1a578b62c22b556e47b6
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-21 14:57:11 -0800

    add fetch tracks actions.

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -4,12 +4,17 @@ import trackerApi from '../api/tracker';
 //from API
 const TrackReducer = (state, action) => {
   switch (action.type) {
+    case 'fetch_tracks':
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async () => {
+  const response = await trackerApi.get('/tracks');
+  dispatch({ type: 'fetch_tracks', payload: response.data });
+};
 const createTrack = dispatch => async (name, locations) => {
   //Make request to API
   await trackerApi.post('/tracks', { name, locations });
